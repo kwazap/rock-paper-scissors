0e337202860a6516f6275716adceb7a55ee5248e
Ivan <kwakiovoono@gmail.com>
2022-08-09 19:18:30 +0200

    Add random computer choice and playing

---
+++ js.js
@@ -0,0 +1,42 @@
+
+
+function getComptuterChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    switch (computerChoice) {
+        case 1:
+            return 'rock'
+        case 2:
+            return 'paper'
+        case 3:
+            return 'scissors'
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        console.log(`It's a tie!`)
+    }
+    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
+        console.log(`You win! Rock beats scissors`)
+    }
+    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
+        console.log(`You lose! Paper beats rock`)
+    }
+    if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
+        console.log(`You win! Paper beats rock`)
+    }
+    if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
+        console.log(`You lose! Scissors beat paper`)
+    }
+    if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
+        console.log(`You win! Scissors beat paper`)
+    }
+    if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
+        console.log(`You lose! Rock beats scissors`)
+    }
+    console.log(playerSelection, computerSelection);
+}
+
+const computerSelection = getComptuterChoice();
+let playerSelection = 'rock'
+
